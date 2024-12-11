ec7a6c7bf94e1026a078b94dfe7d332ab6b7b6ec
nihaashrafc <nihaashh@gmail.com>
2024-12-10 22:36:35 -0800

    Add files via upload

---
+++ maincalc.py
@@ -0,0 +1,7 @@
+import calc
+a=int(input("Enter a:"))
+b=int(input("Enter b:"))
+print(f"addition={calc.add(a,b)}")
+print(f"substraction={calc.subtract(a,b)}")
+print(f"multiplication={calc.multiply(a,b)}")
+print(f"division={calc.divide(a,b)}")
